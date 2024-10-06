19e68ba95411a63311b8e332c29e45f4ee885f1e
Anh Tran <anhtnt@elightup.com>
2024-10-06 15:40:23 +0700

    Initial commit

---
+++ meta-box-lite.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Meta Box Lite
+ * Plugin URI:  https://metabox.io/pricing/
+ * Description: Managing custom post types, custom taxonomies and custom fields easily for WordPress.
+ * Version:     0.0.1
+ * Author:      MetaBox.io
+ * Author URI:  https://metabox.io
+ * License:     GPL2+
+ * Text Domain: meta-box-lite
+ * Domain Path: /languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	return;
+}
+
+// require_once __DIR__ . '/vendor/composer/autoload.php';
